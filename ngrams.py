1b87c85b27c08471112f25858b0ccb6f6164b3e8
sarahParacha <35809671+sarahParacha@users.noreply.github.com>
2020-02-20 16:07:34 -0600

    Update ngrams.py

--- ngrams.py
+++ ngrams.py
@@ -1,7 +1,8 @@
+# Learned from: http://www.albertauyeung.com/post/generating-ngrams-python/
 import nltk, re, string, collections
 from nltk.util import ngrams 
 
-s = "Natural-language processing (NLP) is an area of computer science and artificial intelligence concerned with the interactionsbetween computers and human (natural) languages.";
+s = "Natural-language processing (NLP) is an area of computer science and artificial intelligence concerned with the interactions between computers and human (natural) languages.";
 print(s)
 tokenized = s.split()
 Trigrams = ngrams(tokenized,3)
