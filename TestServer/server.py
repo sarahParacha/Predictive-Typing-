13fb3591b6a346d8f421974e0dcbc332d35d8534
Layne Bernardo <lmbernar@uark.edu>
2020-03-03 18:59:56 -0600

    Added basic dialogue to connect to remote mongoDB

--- TestServer/server.py
+++ TestServer/server.py
@@ -1,11 +1,32 @@
-import pymongo
+import pymongo, getpass, urllib.parse
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-mongoUrl = "localhost"
-print("Connecting to MongoDB on " + mongoUrl)
+if input("Connect to remote mongo server? y/n: ") == "y":
+    mongoBaseUrl = "sorcerodb-9qoxc.mongodb.net/test"
+    mongoUser = input("Enter mongoDB username: ")
+    mongoPass = getpass.getpass("Enter mongoDB password: ")
+    mongoUrl = "mongodb+srv://" + urllib.parse.quote(mongoUser) + ":" + urllib.parse.quote(mongoPass) + "@" + mongoBaseUrl
+    print("Connecting to  MongoDB on " + mongoBaseUrl)
+else:
+    mongoUrl = "localhost"
+    print("Connecting to MongoDB on " + mongoUrl)
+
+try:
+	timeoutDelay = 10000
+	mongoClient = pymongo.MongoClient(mongoUrl, timeoutDelay)
+	# Send request to check connection
+	mongoClient.server_info()
+except pymongo.errors.ServerSelectionTimeoutError as err:
+	print(err)
+	print("Server selection timed out (" + timeoutDelay + " ms)")
+	exit()
+
+print("Connected to mongoDB.")
+db = mongoClient.ngrams
+
 
 mongoClient = pymongo.MongoClient(mongoUrl)
 db = mongoClient.ngrams
@@ -18,8 +39,8 @@ def serve():
 @socketio.on('textInput')
 def textInput(json):
     text = json['data']
-    print('Received text input: ' + text)
-    print()
+    # print('Received text input: ' + text)
+    # print()
     bucket = buckets.find_one({'_id': {"$regex": "^"+text}})
     if bucket is not None:
         return list(bucket)[1:]
